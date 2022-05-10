bbf78018c039fca0ecf77ab04c878ea46967d0c7
drew <sirampr@gmail.com>
2022-05-10 11:34:28 -0400

    Add login page

---
+++ pages/home.tsx
@@ -0,0 +1,12 @@
+import { Container, Title } from "@mantine/core";
+import { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <Container>
+      <Title>Stock App</Title>
+    </Container>
+  );
+};
+
+export default Home;
